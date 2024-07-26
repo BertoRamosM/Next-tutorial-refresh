fda501cb7f68d567c537f6abfa956f44ab2effcf
ARamos <albertramos902@gmail.com>
2024-07-26 13:22:11 +0000

    footer maquetation done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,45 @@
 import React from 'react'
 import styles from "./footer.module.css";
+import Image from 'next/image';
 
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>2024 BertoDev. No rights reserved</div>
 
-
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          alt="facebook"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+        <Image
+          src="/2.png"
+          alt="instagram"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+        <Image
+          src="/3.png"
+          alt="twitter"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+        <Image
+          src="/4.png"
+          alt="youtube"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
