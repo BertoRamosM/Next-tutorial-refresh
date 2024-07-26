3d53654f8b19d80af31a8f0e3b1043b80c23395a
ARamos <albertramos902@gmail.com>
2024-07-26 10:22:34 +0000

    added navbar and footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>2024 BertoDev. No rights reserved</div>
+
+
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
