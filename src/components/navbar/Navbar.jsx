0510f75ddb9d699b684ae9ef739391fd327ea136
ARamos <albertramos902@gmail.com>
2024-07-26 13:12:52 +0000

    global styles and navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 'use client'
 import Link from 'next/link'
 import React from 'react'
+import styles from "./navbar.module.css";
+
 
 const links = [
   {
@@ -38,16 +40,27 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">BertoDev</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        BertoDev
+      </Link>
+      <div className={styles.links}>
         {links.map((link) => (
-          <Link key={link.id} href={link.url}>{link.title}</Link>
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
         ))}
-        <button onClick={()=>{console.log("logout")}}>Logout</button>
+        <button
+          className={styles.logout}
+          onClick={() => {
+            console.log("logout");
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
