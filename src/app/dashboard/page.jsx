16f8a5c3b2c9fcd0836ce154b333d86fdc2c9d14
ARamos <albertramos902@gmail.com>
2024-07-27 16:37:05 +0000

    fetched data with useeffect

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import styles from "./page.module.css";
 
 
+
+
 const Dashboard = () => {
+    /* const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+    const [losding, setLoading] = useState(false);
+
+
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true)
+       const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+         cache: "no-store",
+       });
+
+      if (!res.ok) {
+         setError(true)
+      }
+      const data = await res.json();
+      setData(data)
+      setLoading(false)
+    }
+    getData();
+  }, [])
+ */
+  console.log(data)
+  
+
   return (
     <div className={styles.container}>Dashboard</div>
   )
