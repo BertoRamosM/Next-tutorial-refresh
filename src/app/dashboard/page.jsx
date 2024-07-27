40d9e865a79ec17191a6000fc7e318d3be979559
ARamos <albertramos902@gmail.com>
2024-07-27 16:43:08 +0000

    data fetched with swr

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,13 +1,14 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import styles from "./page.module.css";
+import useSWR from 'swr';
 
 
 
 
 const Dashboard = () => {
 
-  
+
     /* const [data, setData] = useState([]);
   const [error, setError] = useState(false);
     const [losding, setLoading] = useState(false);
@@ -31,12 +32,18 @@ const Dashboard = () => {
   }, [])
  */
   
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+  const {data, error, isLoading} = useSWR(`https://jsonplaceholder.typicode.com/posts` ,fetcher)
+  
 
   console.log(data)
   
 
   return (
-    <div className={styles.container}>Dashboard</div>
+    <div className={styles.container}>
+      
+    </div>
   )
 }
 
